f095db39ca6a776619a97133c688d64756e745cb
Raza <mraza6601@gmail.com>
2022-09-13 13:47:35 +0500

    Home Page completed

---
+++ components/Feed.jsx
@@ -0,0 +1,41 @@
+import { SparklesIcon } from "@heroicons/react/24/outline";
+import Input from "./Input";
+
+import Post from "./Post";
+
+export default function Feed() {
+    const posts = [
+        {
+          id: "1",
+          name: "Muhammad Raza",
+          username: "mraza",
+          userImg: "http://thispix.com/wp-content/uploads/2015/06/Edit-7576-1-300x300.jpg",
+          img: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80",
+          text: "nice view!",
+          timestamp: "2 hours ago",
+        },
+        {
+          id: "2",
+          name: "Muhammad Raza",
+          username: "mraza",
+          userImg: "http://thispix.com/wp-content/uploads/2015/06/Edit-7576-1-300x300.jpg",
+          img: "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
+          text: "wow!",
+          timestamp: "2 days ago",
+        },
+      ];
+  return (
+    <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
+      <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
+        <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
+          <SparklesIcon className="h-5"/>
+        </div>
+      </div>
+      <Input/>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
---
+++ components/Sidebar.jsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import SidebarMenuItem from "./SidebarMenuItem";
+import { HomeIcon  } from "@heroicons/react/24/solid";
+import {
+  BellIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+  HashtagIcon,
+  InboxIcon,
+  UserIcon,
+  Cog6ToothIcon
+  
+} from "@heroicons/react/24/outline";
+export default function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
+      {/* Twitter Logo */}
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
+        <Image
+          width="50"
+          height="50"
+          src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+          alt="Twitter Logo"
+        ></Image>
+      </div>
+
+      {/* Menu */}
+
+      <div className="mt-4 mb-2.5 xl:items-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active/>
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+        <SidebarMenuItem text="Profile" Icon={UserIcon} />
+        <SidebarMenuItem text="More" Icon={Cog6ToothIcon} />
+      </div>
+
+      {/* Button */}
+
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
+
+      {/* Mini-Profile */}
+
+      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+         <Image
+          width="60"
+          height="60"
+          src="http://thispix.com/wp-content/uploads/2015/06/Edit-7576-1-300x300.jpg"
+          alt="User Profile"
+          className="rounded-full xl:mr-2 "
+        ></Image>
+        <div className="leading-5 hidden xl:inline ml-4">
+          <h4 className="font-bold ">Muhammad Raza</h4>
+          <p className="text-gray-500">@mraza</p>
+          <Cog6ToothIcon className="h-5 xl:ml-8 hidden xl:inline"/>
+        </div>
+
+      </div>
+    </div>
+  );
+}
