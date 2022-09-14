1069bea1aa6dc6f2496f34340ea184a9d25b3a9c
Raza <mraza6601@gmail.com>
2022-09-14 21:50:31 +0500

    The creator of tweet can now delete

--- components/Post.jsx
+++ components/Post.jsx
@@ -16,7 +16,8 @@ import {
     onSnapshot,
     setDoc,
   } from "firebase/firestore";
-  import { db } from "../firebase";
+  import { db, storage } from "../firebase";
+  import { deleteObject, ref } from "firebase/storage";
   import { signIn, useSession } from "next-auth/react";
   import { useState, useEffect } from "react";
   
@@ -51,6 +52,16 @@ import {
       signIn();
     }
   }
+
+  async function deletePost() {
+    if (window.confirm("Are you sure you want to delete this post?")) {
+      deleteDoc(doc(db, "posts", post.id));
+      if (post.data().image) {
+        deleteObject(ref(storage, `posts/${post.id}/image`));
+      }
+    }
+  }
+
 
     return (
       <div className="flex p-3 cursor-pointer border-b border-gray-200">
@@ -95,7 +106,12 @@ import {
   
           <div className="flex justify-between text-gray-500 p-2">
             <ChatBubbleLeftIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
-            <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+            {session?.user.uid === post?.data().id && (
+            <TrashIcon
+              onClick={deletePost}
+              className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+            />
+          )}
             <div className="flex items-center">
             {hasLiked ? (
               <HeartIconFilled
