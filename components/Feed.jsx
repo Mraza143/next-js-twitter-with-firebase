d4e905d8a513fd7a201cc4480dc97e4afdd37e04
Raza <mraza6601@gmail.com>
2022-09-15 17:26:09 +0500

    deployment done

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,10 +4,12 @@ import { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { db } from "../firebase";
 import Input from "./Input";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 import Post from "./Post";
 
 export default function Feed() {
+  const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
   useEffect(
     () =>
@@ -23,6 +25,12 @@ export default function Feed() {
     <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
         <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
+        {!session? <button
+          onClick={signIn}
+          className="bg-blue-400 text-white ml-auto rounded-full w-24 h-8 font-bold shadow-md hover:brightness-95 text-lg  md:hidden"
+        >
+          Sign in
+        </button> : ""}
         <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
           <SparklesIcon className="h-5"/>
         </div>
